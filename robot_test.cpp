87345efbff1106c6f3ac0df0da4c5f0e1bae9496
WangYenHsiang <89662813+WangYenHsiang@users.noreply.github.com>
2021-10-12 19:37:07 -0400

    Create robot_test.cpp

---
+++ robot_test.cpp
@@ -0,0 +1,19 @@
+/*
+    Author:Wang,YenHsiang
+    version : 1.0
+    date : Oct/11
+*/
+#include "robot.hh"
+//#include "robot.cc"
+#include<iostream>
+using namespace std;
+
+extern vec_3d estlocation;
+
+int main(){
+    robot frank(2, 4, 9, 90);
+    cout<<"frank location="<<frank;
+    estlocation = frank.dist(TestBeacon);
+    cout<<estlocatioin<<endl;
+}
+
